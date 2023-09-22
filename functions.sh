0d8404b95e11ac3218b682a1aa4a0f34496fbcc1
James Lochhead <james@lochhead.me>
2023-09-22 22:12:13 +0100

    feat: various features
    * Functionality for AWS_REGION
    * Functionality for current working directory
    * Functionality for spacing commands
    * Improved unset/source behaviour to avoid bugs outside this script

--- functions.sh
+++ functions.sh
@@ -4,7 +4,9 @@ set_text_foreground_color() {
 	# Use https://bash-prompt-generator.org/ to work out what integer you
 	# need. If $5 is present and true, the text is bold. Call this in a
 	# a subshell and store the result in a variable.
-	if [[ $5 == true ]]; then
+	local _BOLD
+	_BOLD=$(tput bold)
+	if [[ "$5" == true ]]; then
 		printf "\[\e[0;1;%s;%s;%sm\]\[$_BOLD\]%s\[\e[0m\]" "$2" "$3" "$4" "$1"
 	else
 		printf "\[\e[0;1;%s;%s;%sm\]%s\[\e[0m\]" "$2" "$3" "$4" "$1"
@@ -12,3 +14,45 @@ set_text_foreground_color() {
 }
 
 export set_text_foreground_color
+
+current_working_directory() {
+	# If the absolute path to the current working directory contains
+	# the path to $HOME, replace it with ~. If it's exactly the $HOME
+	# directory, replace that with the absolute path to $HOME. Print
+	# the result.
+	local result
+	result="$(pwd | sed "s|$HOME|~|")"
+	if [[ "$result" == "~" ]]; then
+		result="$HOME"
+	fi
+	printf "%s" "$result"
+}
+
+export current_working_directory
+
+aws_region() {
+	# Print the current AWS region if it's set
+	if [[ -v AWS_REGION ]]; then
+		printf "%s" "$AWS_REGION"
+	fi
+}
+
+export aws_region
+
+space_if_not_empty() {
+	# Add a space in front of $1 if it's not empty, then print it
+	if [[ -n "$1" ]]; then
+		printf " %s" "$1"
+	fi
+}
+
+export space_if_not_empty
+
+rc() {
+	# If $1 isn't 0, print it. Intended for the last return code.
+	if (( "$1" != 0 )); then
+		printf "%s" "$1"
+	fi
+}
+
+export rc
--- my_prompt_command.sh
+++ my_prompt_command.sh
@@ -1,15 +1,32 @@
-source "$HOME/Personal.Git/secure-bash-prompt/functions.sh"
-
 prompt_command() {
+	LAST_RC="$?"
+	source "$HOME/Personal.Git/secure-bash-prompt/functions.sh"
+
+	local GREATER_THAN; local HOST_NAME; local WORKING_DIR;
+	local FIRST_LINE; local SECOND_LINE; local SBP_AWS_REGION
+
+	space_and_colour() {
+		printf "%s" "$(space_if_not_empty "$(set_text_foreground_color "$1" "$2" "$3" "$4" "$5")")"
+	}
+
 	GREATER_THAN="$(set_text_foreground_color ">" "38" "5" "33" true)"
 	HOST_NAME="$(set_text_foreground_color "\h" "38" "5" "33" true)"
-	WORKING_DIR=$(set_text_foreground_color "\w" "38" "5" "33" true)
+	NOCOLOR_CWD="$(current_working_directory)"
+	WORKING_DIR=$(set_text_foreground_color "$NOCOLOR_CWD" "38" "5" "33" true)
+	SBP_AWS_REGION="$(space_and_colour "$(aws_region)" "0" "0" "0" true)"
+	RC="$(space_and_colour "$(rc $LAST_RC)" "0" "0" "0" true)"
 
-	FIRST_LINE="$HOST_NAME $WORKING_DIR"
+	FIRST_LINE="$HOST_NAME $WORKING_DIR$SBP_AWS_REGION$RC"
 	SECOND_LINE="$GREATER_THAN "
 
 	PS1="\n$FIRST_LINE\n$SECOND_LINE"
 	export PS1
+
+	unset -f set_text_foreground_color
+	unset -f current_working_directory
+	unset -f aws_region
+	unset -f space_if_not_empty
+	unset -f rc
 }
 
 PROMPT_COMMAND=prompt_command
